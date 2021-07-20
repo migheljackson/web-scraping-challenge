c1134571d82fde93c73916f6be22cdda20841bd4
migheljackson <10728980+migheljackson@users.noreply.github.com>
2021-07-19 23:11:47 -0500

    Added featured image code to scrape script

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -17,15 +17,22 @@ def scrape():
 
     # initiating the webdriver. Parameter includes the path of the webdriver.
     driver = webdriver.Chrome('chromedriver.exe') 
+    
+    #Get newest article
     driver.get(news_url)
-
+    
     time.sleep(5)
-
+    
     html = driver.page_source
     soup = BeautifulSoup(html,"html.parser")
 
-    #Get newest article
+
     articles=soup.find('div'),class_="list_text")
-    newest_date = articles.find('div',class_='list_date')
-    newest_title = articles.find('div',class_='list_date')
-    newest_blurb = articles.find('div',class_='list_date')
+    newest_date = articles.find('div',class_='list_date').text
+    newest_title = articles.find('div',class_='content_title').text
+    newest_blurb = articles.find('div',class_='article_teaser_body').text
+
+    #Get featured space image
+    driver.get(space_image_url)
+    html = driver.page_source
+    soup = BeautifulSoup(html,"html.parser")
